cd8760b273f733b1d63ebc94d92b907440c70d8b
josephkeinan <yk3823@gmail.com>
2025-03-31 20:33:18 +0300

    check change in port

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "Starting Python HTTP server on port 5003..."
-exec python3 -m http.server 5003 --directory /app
+echo "Starting Python HTTP server on port 8000..."
+exec python3 -m http.server 8000 --directory /app
 
