3b3ae7b11ff09e84727858c9fe2d2fcbbf437833
josephkeinan <yk3823@gmail.com>
2025-04-06 20:38:13 +0300

    dj

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "Starting Python HTTP server on port 6003..."
-exec python3 -m http.server 6003 --directory /app
+echo "Starting Python HTTP server on port 6005..."
+exec python3 -m http.server 6005 --directory /app
 
